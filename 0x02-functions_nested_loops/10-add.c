b0e733fb74739d790f77eb565600fd922a2b160b
MbuguaKariuki <Kmbugua003@gmail.com>
2023-03-21 18:29:07 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * add-sum sum
+ * Return: retuning the val
+ * @a:parameter
+ * @b:parameter
+*/
+
+int add(int a, int b)
+{
+int sum;
+sum = a + b;
+return (sum);
+
+}
