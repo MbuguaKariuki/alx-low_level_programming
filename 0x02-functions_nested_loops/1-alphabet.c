9218ca606f313a3807ce6be8242a770a7d94478f
MbuguaKariuki <Kmbugua003@gmail.com>
2023-03-21 17:42:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabetic
+ * Return: Always (success)
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+}
